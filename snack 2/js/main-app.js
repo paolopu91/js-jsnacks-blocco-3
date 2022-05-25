e79f337252ee5b10cc433b82d10fa8a47bc543af
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-25 16:49:42 +0200

    aggiunta struttura iniziale snack 3

--- snack 2/js/main-app.js
+++ snack 2/js/main-app.js
@@ -4,3 +4,11 @@ Esercizio numero 2
  Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. 
  Ogni volta che ne crei uno, stampalo.
 */
+
+// creo il mio array
+let array = [];
+
+// costante con cui il mio utente inserirà il suo numero
+const askUtente = parseInt(prompt("Inserisci qui un numero"))
+console.log(askUtente)
+
