25cea7f112731218f79d98cfff3f73e5510a472a
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-25 15:51:50 +0200

    aggiunta richiesta esercizio

---
+++ snack 2/js/main.js
@@ -0,0 +1,6 @@
+/*Snack2
+Esercizio numero 2
+-Fai inserire un numero, che chiameremo N, all’utente. 
+ Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. 
+ Ogni volta che ne crei uno, stampalo.
+*/
