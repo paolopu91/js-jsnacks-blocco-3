c76eabd949df05cb47b9529d1180d6ddcac833cc
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-26 14:08:09 +0200

    aggiunta modifica snack 2

--- snack 2/js/main-app.js
+++ snack 2/js/main-app.js
@@ -5,10 +5,29 @@ Esercizio numero 2
  Ogni volta che ne crei uno, stampalo.
 */
 
-// creo il mio array
-let array = [];
 
-// costante con cui il mio utente inserirà il suo numero
-const askUtente = parseInt(prompt("Inserisci qui un numero"))
-console.log(askUtente)
+// chiedo all'utente di darmi un numero
+const askUtente = parseInt(prompt("Inserisci qui un numero qualsiasi"));
+// dovremo validare il numero inserito
 
+const arrayGenerati = [];
+
+// finche la lunghezza di array generati è < del numero chiesto all'utente, ripeto il ciclo
+while(arrayGenerati.length < askUtente){
+// creo il nuovo array
+    const nuovoArray = [];
+
+ // continuo ad inserire numeri finchè non siamo arrivati a 10 numeri
+while(nuovoArray.length < 10){
+    const numeriCasuali = Math.floor(Math.random() * 100) + 1;
+
+    if(!nuovoArray.includes(numeriCasuali)){
+        
+        nuovoArray.push(numeriCasuali);
+    }
+    
+}
+// aggiunge l'array generato alla lista degli array richiamata fuori
+arrayGenerati.push(nuovoArray);
+
+}
